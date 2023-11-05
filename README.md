99cdff5527616ba66565e628baa485b78fd39537
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2023-11-05 14:12:33 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# RedShelf-Bypass-PDFDownload
-RedShelf Virdocs - Bypass protection PDF Download. Download ePub/eReader documents to PDF when downloading and printing is not allowed.
+# RedShelf Virdocs - Bypass protection PDF Download
+
+In the web platform "**RedShelf Virdocs**" you are allowed to print in PDF your ePub/eReader documents, but sometimes contributors do not allow downloading and printing to avoid possible leaks or third party distributions not allowed.
+
+This is a small Python script that allows you to "bypass" this protection and download ePub/eReader documents and export them to PDF format. First all pages are downloaded in jpg image format, resized to A4 size and finally a PDF document output is generated.
+
+### Requirements
+```python
+pip install requests
+pip install reportlab
+```
+
+### Get URL ePub/eReader images
+Inside the RedShelf Virdocs ePub/eReader viewer. We inspect the web and look for the iframe corresponding to the current page, in the body we will see a div containing the internal URL inside an img src tag. There we can copy the absolute path and substitute it in the variable "url_base".
+
+![redshelf-virdocs-url-img](screenshots/redshelf-virdocs-url-img.png)
+
+### Variables
+- Indicates the directory where the jpg images will be saved and the name of the exported PDF file.
+- Replace XXXXXXX with the value of the URL.
+- Replace the numpag variable with the number of jpg URL pages contained in the document.
+```python
+target_directory_img = "TARGET_DIRECTORY_IMG"
+base_url = "https://platform.virdocs.com/rscontent/epub/XXXXXXX/XXXXXXX/OEBPS/images/page-{}.jpg"
+pdf_file = "EXPORT_PDF_FILE.pdf"
+numpag = 350
+```
+
+### Cookies
+Replace session cookie settings. Use any browser addon for editing and exporting cookies.
+```json
+{
+    "session_id": "value1",
+    "csrftoken": "value2"
+}
+```
+
+### Usage
+```python
+python3 RedShelf-Bypass-PDFDownload.py
+```
---
+++ RedShelf-Bypass-PDFDownload.py
@@ -0,0 +1,68 @@
+import requests
+import json
+import os
+from reportlab.lib.pagesizes import A4
+from reportlab.lib import utils
+from reportlab.platypus import SimpleDocTemplate, Image
+
+# Load session cookies from a JSON file
+def load_cookies_from_json(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            cookies = json.load(file)
+        return cookies
+    except FileNotFoundError:
+        print(f"The file {file_name} was not found.")
+        return None
+
+# Directory in which you want to save the images
+target_directory_img = "TARGET_DIRECTORY_IMG"
+
+# Create the directory if it does not exist
+if not os.path.exists(target_directory_img):
+    os.makedirs(target_directory_img)
+
+# Base URL
+base_url = "https://platform.virdocs.com/rscontent/epub/XXXXXXX/XXXXXXX/OEBPS/images/page-{}.jpg"
+
+# Load session cookies from a JSON file
+cookies = load_cookies_from_json("cookies.json")
+
+if cookies is not None:
+    # PDF file name
+    pdf_file = "EXPORT_PDF_FILE.pdf"
+
+    # Create an A4 formatted PDF document in portrait orientation
+    doc = SimpleDocTemplate(pdf_file, pagesize=A4, rightMargin=30, leftMargin=30, topMargin=30, bottomMargin=30)
+
+    images = []
+    numpag = 350
+    
+    for numpag in range(1, numpag + 1):
+        url = base_url.format(numpag)
+        file_name = os.path.join(target_directory_img, f"{numpag}.jpg")
+
+        # HTTP request with session cookies
+        response = requests.get(url, cookies=cookies)
+
+        if response.status_code == 200:
+            with open(file_name, 'wb') as imagen:
+                imagen.write(response.content)
+            print(f"Image {file_name} successfully downloaded.")
+
+            # Add image to PDF document
+            img = utils.ImageReader(file_name)
+            img_width, img_height = img.getSize()
+            aspect_ratio = img_height / float(img_width)
+            images.append((numpag, Image(file_name, width=A4[0], height=(A4[0] * aspect_ratio))))
+        else:
+            print(f"Could not download the {file_name}. Status code: {response.status_code}")
+
+    # Sort images by page number
+    images = sorted(images, key=lambda x: x[0])
+
+    # Generate the PDF with the images in page order
+    doc.build([img for _, img in images])
+    print(f"PDF created: {pdf_file}")
+else:
+    print("Could not load session cookies.")
